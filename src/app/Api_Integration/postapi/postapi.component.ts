ad5b6a9d546058a68f82e0260381e08024120622
Harshada Bhondave <harshadabhondave09@gmail.com>
2024-08-04 14:43:22 +0530

    service for api

--- src/app/Api_Integration/postapi/postapi.component.ts
+++ src/app/Api_Integration/postapi/postapi.component.ts
@@ -1,16 +1,21 @@
 import { Component, inject ,OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+import { ApiService } from '../../service/api.service';
 
 @Component({
   selector: 'app-postapi',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,JsonPipe],
   templateUrl: './postapi.component.html',
   styleUrl: './postapi.component.css'
 })
 export class PostapiComponent implements OnInit {
   http = inject(HttpClient)
+
+constructor(private deptservice: ApiService){}
+
   departObj:any ={
     "departmentId":0,
     "departmentName":"",
@@ -19,22 +24,71 @@ export class PostapiComponent implements OnInit {
   deptList:any[]=[]
 
   ngOnInit(): void {
-   this.getDeapartment()   
+   this.getDepartment()   
   }
 
-  onSave(){
+  /* onSave(){
     this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment",this.departObj).subscribe((res:any)=>{
       if(res.result){
         alert("user created successfully")
+        this.getDepartment()
       }else{
         alert(res.message)
       }
     })
+  } */
+ onSave(){
+  this.deptservice.saveNewDept(this.departObj).subscribe((res:any)=>{
+    if(res.result){
+      alert("user created successfully")
+      this.getDepartment()
+    }else{
+      alert(res.message)
+    }
+    this.departObj.departmentId = 0;
+    this.departObj.departmentName = "";
+    this.departObj.departmentLogo = ""
+  })
+ }
+
+  onEdit(data:any){
+    this.departObj = data;
   }
 
-  getDeapartment(){
-    this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment").subscribe((res:any)=>{
-     this.deptList = res.data
+  onUpdate(){
+    this.deptservice.updateDept(this.departObj).subscribe((res:any)=>{
+      if(res.result){
+        alert("user updated")
+        this.getDepartment()
+      }else{
+        alert(res.message)
+      }
+      this.departObj = ""
+        
+    })
+  }
+
+  onDelete(id:number){
+    this.http.delete("https://projectapi.gerasim.in/api/Complaint/DeletedepartmentBydepartmentId?departmentId="+id).subscribe((res:any)=>{
+      if(res.result){
+        alert("user deleted")
+        this.getDepartment()
+      }else{
+        alert(res.message)
+      }
+    })
+
+  }
+
+  // getDeapartment(){
+  //   this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment").subscribe((res:any)=>{
+  //    this.deptList = res.data
+  //   });
+  // }
+
+  getDepartment(){
+    this.deptservice.getAllDept().subscribe((res:any)=>{
+      this.deptList = res.data
     });
   }
 }
---
+++ src/app/service/api.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  getAllDept(){
+   return this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment");
+  }
+
+  saveNewDept(obj:any){
+    return this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment",obj);
+  }
+
+  updateDept(obj:any){
+    return this.http.post("https://projectapi.gerasim.in/api/Complaint/UpdateDepartment",obj)
+  }
+  
+}
+
+  
